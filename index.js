2f13ee86d924058de20db5a5de2ef5da148fc943
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-07-04 12:30:55 -0700

    Done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  const box = document.getElementById("game");
+
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+    const boxWidth = box.offsetWidth;
+    const dodgerWidth = dodger.offsetWidth;
+    const maxRight = boxWidth - dodgerWidth;
+
+    if (left <= maxRight) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
